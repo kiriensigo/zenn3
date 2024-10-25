23340141efcf16793c73e4320fc980573ce3fc77
kiriensigo <kiriensigo@gmail.com>
2024-10-26 05:31:49 +0900

    add button

---
+++ next/src/components/SimpleButton.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const SimpleButton: React.FC = () => {
+  const handleClick = () => {
+    console.log("Clicked!");
+  };
+  return <button onClick={handleClick}>Click Me</button>;
+};
+
+export default SimpleButton;
---
+++ next/src/pages/hello_world.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+import SimpleButton from "@/components/SimpleButton";
+
+const HelloWorld: NextPage = () => {
+  return (
+    <>
+      <h1>Title</h1>
+      <p>content</p>
+      <SimpleButton />
+    </>
+  );
+};
+
+export default HelloWorld;

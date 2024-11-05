7acde5653241ed1122e7235dbb6ebeda7150e410
kiriensigo <kiriensigo@gmail.com>
2024-11-05 23:25:19 +0900

    15.end

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
     namespace :v1 do
       get "health_check", to: "health_check#index"
       mount_devise_token_auth_for "User", at: "auth"
+
+      namespace :current do
+        resource :user, only: [:show]
+      end
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

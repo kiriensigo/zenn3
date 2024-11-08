60ab26b70425981c0b319e253f7b2e45f5cae45b
kiriensigo <kiriensigo@gmail.com>
2024-11-09 00:15:27 +0900

    16.end

--- rails/spec/models/user_spec.rb
+++ rails/spec/models/user_spec.rb
@@ -2,11 +2,11 @@ require "rails_helper"
 
 RSpec.describe User, type: :model do
   context "factoryのデフォルト設定に従った場合" do
-    let(:user) { create(:user) }
+    let(:user) { create(:user) }  # FactoryBotを使用してユーザーを作成
 
     it "認証済みの user レコードを正常に新規作成できる" do
-      expect(user).to be_valid
-      expect(user).to be_confirmed
+      expect(user).to be_valid          # ユーザーが有効であることを確認
+      expect(user).to be_confirmed      # ユーザーが認証済みであることを確認
     end
   end
 end

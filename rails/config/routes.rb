66c303f3c20cd4607cbe4e57ac1e551a6f940385
kiriensigo <kiriensigo@gmail.com>
2024-11-11 00:53:21 +0900

    18end fixed

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
       namespace :current do
         resource :user, only: [:show]
       end
+      resources :articles, only: [:index, :show]
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

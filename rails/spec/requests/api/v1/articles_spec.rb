130fa7186146a8c733ba5534e223ac86f11bfc85
kiriensigo <kiriensigo@gmail.com>
2024-11-13 23:38:16 +0900

    19.3

--- rails/spec/requests/api/v1/articles_spec.rb
+++ rails/spec/requests/api/v1/articles_spec.rb
@@ -17,8 +17,8 @@ RSpec.describe "Api::V1::Articles", type: :request do
         res = JSON.parse(response.body)
         expect(res.keys).to eq ["articles", "meta"]
         expect(res["articles"].length).to eq 10
-        expect(res["articles"][0].keys).to eq ["id", "title", "content", "created_at", "from_today", "user"]
-        expect(res["articles"][0]["user"].keys).to match_array(["id", "name"])  # 修正
+        expect(res["articles"][0].keys).to eq ["id", "title", "content", "status", "created_at", "from_today", "user"]
+        expect(res["articles"][0]["user"].keys).to match_array(["id", "name"])
         expect(res["meta"].keys).to eq ["current_page", "total_pages"]
         expect(res["meta"]["current_page"]).to eq 1
         expect(res["meta"]["total_pages"]).to eq 3
@@ -34,7 +34,7 @@ RSpec.describe "Api::V1::Articles", type: :request do
         res = JSON.parse(response.body)
         expect(res.keys).to eq ["articles", "meta"]
         expect(res["articles"].length).to eq 10
-        expect(res["articles"][0].keys).to eq ["id", "title", "content", "created_at", "from_today", "user"]
+        expect(res["articles"][0].keys).to eq ["id", "title", "content", "status", "created_at", "from_today", "user"]
         expect(res["articles"][0]["user"].keys).to match_array(["id", "name"])  # 修正
         expect(res["meta"].keys).to eq ["current_page", "total_pages"]
         expect(res["meta"]["current_page"]).to eq 2
@@ -58,7 +58,7 @@ RSpec.describe "Api::V1::Articles", type: :request do
         it "正常にレコードを取得できる" do
           subject
           res = JSON.parse(response.body)
-          expect(res.keys).to eq ["id", "title", "content", "created_at", "from_today", "user"]
+          expect(res.keys).to eq ["id", "title", "content", "status", "created_at", "from_today", "user"]
           expect(res["user"].keys).to match_array(["id", "name"])  # 修正
           expect(response).to have_http_status(:ok)
         end

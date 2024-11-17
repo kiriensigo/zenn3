163436c2bd5c23985370a6352073f5a275866570
kiriensigo <kiriensigo@gmail.com>
2024-11-17 20:50:24 +0900

    23.1

--- rails/app/controllers/api/v1/current/articles_controller.rb
+++ rails/app/controllers/api/v1/current/articles_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::Current::ArticlesController < Api::V1::BaseController
     before_action :authenticate_user!
 
+    def create
+      unsaved_article = current_user.articles.unsaved.first || current_user.articles.create!(status: :unsaved)
+      render json: unsaved_article
+    end
+
     def index
         articles = current_user.articles.not_unsaved.oder(created_at: :desc)
         render json: articles

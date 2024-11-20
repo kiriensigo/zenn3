02cee5e60b725ea12acf39d942971d9f9e1edaf2
kiriensigo <kiriensigo@gmail.com>
2024-11-20 23:29:24 +0900

    26.1

--- next/src/components/Header.tsx
+++ next/src/components/Header.tsx
@@ -1,24 +1,24 @@
-import { AppBar, Box, Button, Container } from '@mui/material'
-import Image from 'next/image'
-import Link from 'next/link'
+import { AppBar, Box, Button, Container } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <AppBar
       position="static"
       sx={{
-        backgroundColor: 'white',
-        color: 'black',
-        boxShadow: 'none',
-        py: '12px',
+        backgroundColor: "white",
+        color: "black",
+        boxShadow: "none",
+        py: "12px",
       }}
     >
       <Container maxWidth="lg" sx={{ px: 2 }}>
         <Box
           sx={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
           }}
         >
           <Box>
@@ -27,28 +27,30 @@ const Header = () => {
             </Link>
           </Box>
           <Box>
-            <Button
-              color="primary"
-              variant="contained"
-              sx={{
-                color: 'white',
-                textTransform: 'none',
-                fontSize: 16,
-                borderRadius: 2,
-                boxShadow: 'none',
-              }}
-            >
-              Sign in
-            </Button>
+            <Link href="/sign_in" passHre>
+              <Button
+                color="primary"
+                variant="contained"
+                sx={{
+                  color: "white",
+                  textTransform: "none",
+                  fontSize: 16,
+                  borderRadius: 2,
+                  boxShadow: "none",
+                }}
+              >
+                Sign in
+              </Button>
+            </Link>
             <Button
               color="primary"
               variant="outlined"
               sx={{
-                textTransform: 'none',
+                textTransform: "none",
                 fontSize: 16,
                 borderRadius: 2,
-                boxShadow: 'none',
-                border: '1.5px solid #3EA8FF',
+                boxShadow: "none",
+                border: "1.5px solid #3EA8FF",
                 ml: 2,
               }}
             >
@@ -58,7 +60,7 @@ const Header = () => {
         </Box>
       </Container>
     </AppBar>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
